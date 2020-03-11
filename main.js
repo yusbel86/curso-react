33ce8b2dff31dfaad8e750ade42bbc9180f844a8
yusbel86 <yusbel1086@gmail.com>
2020-03-10 18:03:22 -0700

    herencia

---
+++ main.js
@@ -0,0 +1,24 @@
+class Human {
+    constructor(){
+        this.gender = 'male';
+    }
+
+    printMygender(){
+        console.log(this.gender);
+    }
+}
+
+class Person extends Human {
+    constructor(){
+        super();
+        this.name = 'Yusbel';
+    }
+
+    printMyname(){
+        console.log(this.name);
+    }
+}
+
+const person = new Person();
+person.printMyname(); 
+person.printMygender();
