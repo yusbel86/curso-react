333e91eb030aaac71f90315315cc71c21ab71034
yusbel86 <yusbel1086@gmail.com>
2020-03-23 15:43:02 -0700

    States

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -42,7 +42,7 @@ class App extends Component {
  render(){  
   return (
     <div className="App">
-     <h1>Hi, I am Yusbel 
+     <h1>Hi, I am Yusbel Carpio
        <button 
            onClick={this.togglePersonHandler}>Switch Name</button>
        </h1>
